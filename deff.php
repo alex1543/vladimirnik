82265b907b6829d7edf0e95f02bbca656cef3b1b
Alexey Subbotin <alesu1543@gmail.com>
2023-04-21 21:16:52 +0300

    fix script

--- deff.php
+++ deff.php
@@ -1,26 +1,24 @@
 ﻿<?php
 
-			global $MySQLServer;
-			global $MyUser;
-			global $MyUserTable;
-			global $MyPasswordSQL;
+	global $MySQLServer;
+	global $MyUser;
+	global $MyUserTable;
+	global $MyPasswordSQL;
 
-				$MyPWDEff = "8940";
-				$MyFileName = "MyPW.txt";
-				$MySpecValues = 10;
+	$MyPWDEff = "8940";
+	$MyFileName = "MyPW.txt";
+	$MySpecValues = 10;
 				
-			$MySQLServer = "mysql.hostinger.ru";
-			$MyUser = "u296292563_user";
-			$MyUserTable = "u296292563_user";
-			$MyPasswordSQL = "123456789q";
+	$MySQLServer = "mysql.hostinger.ru";
+	$MyUser = "u296292563_user";
+	$MyUserTable = "u296292563_user";
+	$MyPasswordSQL = "123456789q";
 
-				if ($_SERVER['SERVER_NAME'] == "localhost") {
-					$MySQLServer = "localhost";
-					$MyUser = "root";
-					$MyUserTable = "vladimirnik";
-					$MyPasswordSQL = "";				
-				}
-				
-				
+		if ($_SERVER['SERVER_NAME'] == "localhost") {
+			$MySQLServer = "localhost";
+			$MyUser = "root";
+			$MyUserTable = "vladimirnik";
+			$MyPasswordSQL = "";				
+		}
 ?>
 
